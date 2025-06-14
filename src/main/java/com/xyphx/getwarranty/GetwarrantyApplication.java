4fdbfb7df66f48811c7b29b989448604dbe52a15
S SIDHARTH <sidharthpunalur@gmail.com>
2025-06-14 17:06:58 +0530

    Test endpoint created

---
+++ src/main/java/com/xyphx/getwarranty/GetwarrantyApplication.java
@@ -0,0 +1,13 @@
+package com.xyphx.getwarranty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetwarrantyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetwarrantyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xyphx/getwarranty/GetwarrantyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyphx.getwarranty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetwarrantyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
